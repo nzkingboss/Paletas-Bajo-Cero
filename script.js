a9d18ccc0dae0e5b1a5c00a15ba2ccc312537608
nzkingboss <junior.jrrp@gmail.com>
2024-01-04 19:12:36 -0500

    Responsive Home

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// SCROLL MENU
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -13,11 +15,32 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
 }
 
+// REDIRIGIR CON BOTONES
+
 function irASucursales() {
-  // Redirigir a la página "sucursales.html"
   window.location.href = 'sucursales.html';
 }
 
 function redireccionar() {
   window.location.href = "menu.html"; 
 } 
+
+// MENU RESPONSIVE
+
+document.getElementById ("btn_menu").addEventListener("click", mostrar_menu);
+document.getElementById ("back_menu").addEventListener("click", ocultar_menu);
+
+nav = document.getElementsByClassName("header-list-nav")[0];
+background_menu = document.getElementById("back_menu");
+
+function mostrar_menu() { 
+  nav.style.right = "0px";
+  background_menu.style.display = "block";
+} 
+
+function ocultar_menu() { 
+  nav.style.right = "-250px";
+  background_menu.style.display = "none";
+} 
+
+ 
